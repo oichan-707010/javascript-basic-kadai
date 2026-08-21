f18d69f589514f938a68893fa220aa80f6b65e4b
oichan-707010 <naotooikawa7010@gmail.com>
2026-08-21 17:05:32 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,5 @@
+const currently = new Date();
+const nowYear = currently.getFullYear();
+const nowMonth = currently.getMonth() + 1;
+const nowDate = currently.getDate();
+console.log(nowYear + "年" + nowMonth + "月" + nowDate + "日");
