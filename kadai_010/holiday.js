07c0add6342a7aacc8252922be5dcb5b5adc779c
oichan-707010 <naotooikawa7010@gmail.com>
2026-08-21 15:16:03 +0900

    Add files via upload

---
+++ kadai_010/holiday.js
@@ -0,0 +1,32 @@
+const holidays = [
+  "正月",
+  "成人の日",
+  "建国記念日",
+  "天皇誕生日",
+  "春分の日",
+  "昭和の日",
+  "憲法記念日",
+  "みどりの日",
+  "こどもの日",
+  "海の日",
+  "山の日",
+  "敬老の日",
+  "秋分の日",
+  "スポーツの日",
+  "文化の日",
+  "勤労感謝の日",
+];
+
+// console.log(holidays.length);
+
+// for文の場合
+for (i = 0; i <= 15; i++) {
+  console.log(holidays[i]);
+}
+
+// while文の場合
+let arrayNumber = 0;
+while (arrayNumber < holidays.length) {
+  console.log(holidays[arrayNumber]);
+  arrayNumber++;
+}
