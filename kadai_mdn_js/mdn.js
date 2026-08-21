78c00f3e99e36312fe914fecbe31f564c4d87b2b
oichan-707010 <naotooikawa7010@gmail.com>
2026-08-21 17:05:16 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,5 +0,0 @@
-const currently = new Date();
-const nowYear = currently.getFullYear();
-const nowMonth = currently.getMonth();
-const nowDate = currently.getDate();
-console.log(nowYear + "年" + nowMonth + "月" + nowDate + "日");
